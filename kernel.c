43d8a7cea077c902655383f1893009ec88a7fca3
Tenghoit Kouch <tenghoitkouch@gmail.com>
2026-04-08 14:50:35 -0400

    setback

--- kernel.c
+++ kernel.c
@@ -6,28 +6,62 @@ int printChar(char character, int color, int line, int col){
     int totalLines = 25;
     int charSize = 2;
 
-    int relativePosition = charPerLine * line + col;
-    relativePosition *= charSize;
+    int offset = (charPerLine * line + col) * charSize;
 
-    int absolutePosition = videoMemoryOrigin + relativePosition;
-
-    int segment = (int)(absolutePosition / 0x1000);
+    /*
+    int absolutePosition = videoMemoryOrigin + offset;
     int remainingAddress = (int)(absolutePosition % 0x1000);
+    */ 
 
-    putInMemory(0xB000, remainingAddress, character);
-    putInMemory(0xB000, remainingAddress + 1, color);
+    int segment = 0xB000;
+    int remaining = 0x8000;
+
+    putInMemory(0xB000, remaining, character);
+    putInMemory(0xB000, remaining+1, color);
+}
+
+void printHelloWorld(){
+    putInMemory(0xB000, 0x80A0, 'H');
+    putInMemory(0xB000, 0x80A1, 0x7);
+   
+    putInMemory(0xB000, 0x80A2, 'E');
+    putInMemory(0xB000, 0x80A3, 0x7);
+
+    putInMemory(0xB000, 0x80A4, 'L');
+    putInMemory(0xB000, 0x80A5, 0x7);
+
+    putInMemory(0xB000, 0x80A6, 'L');
+    putInMemory(0xB000, 0x80A7, 0x7);
+
+    putInMemory(0xB000, 0x80A8, 'O');
+    putInMemory(0xB000, 0x80A9, 0x7);
+
+ 
+
+    putInMemory(0xB000, 0x80AA, 'W');
+    putInMemory(0xB000, 0x80AB, 0x7);
+
+    putInMemory(0xB000, 0x80AC, 'O');
+    putInMemory(0xB000, 0x80AD, 0x7);
+
+    putInMemory(0xB000, 0x80AE, 'R');
+    putInMemory(0xB000, 0x80AF, 0x7);
+
+    putInMemory(0xB000, 0x80B0, 'L');
+    putInMemory(0xB000, 0x80B1, 0x7);
+
+    putInMemory(0xB000, 0x80B2, 'D');
+    putInMemory(0xB000, 0x80B3, 0x7);
+
 }
 
 void main()
 {
-    int videoMemoryOrigin = 0xB8000;
-
-    int charPerLine = 80;
-    int totalLines = 25;
-    int charSize = 2;
-
     int AasciiCode = 0x41;
     int white = 0x7;
+     
+ 
+    putInMemory(0xB000, 0x8140, 'A');
+    putInMemory(0xB000, 0x8141, 0x7);
 
-    printChar("A", white, 0, 0);
 }
